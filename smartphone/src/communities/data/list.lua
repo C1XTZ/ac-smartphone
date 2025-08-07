4aa1fb699444aaad3312a59455e06291abdb4ef2
C1XTZ <C1XTZ@proton.me>
2025-08-08 00:45:51 +0200

    up version

--- smartphone/src/communities/data/list.lua
+++ smartphone/src/communities/data/list.lua
@@ -1,6 +1,6 @@
 {
   version = { 28052025 },
-  default = { url = "https://github.com/C1XTZ/ac-smartphone", text = "Smartphone App on Github", image = ".\\apps\\lua\\smartphone\\src\\img\\default.png" }
+  default = { url = "https://github.com/C1XTZ/ac-smartphone", text = "Smartphone App on Github", image = ".\\apps\\lua\\smartphone\\src\\img\\default.png" },
   srp = {
     ips = { "5.161.43.117", "65.108.176.35", "15.235.162.98" },
     url = "https://discord.gg/shutokorevivalproject",
